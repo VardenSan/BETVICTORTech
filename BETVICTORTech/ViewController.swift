b5f7f162915ac9b2492f9f337fe5cf06efff770e
sergioTimp <sergio@timp.pro>
2022-03-17 20:12:07 +0100

    Initial Commit

---
+++ BETVICTORTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BETVICTORTech
+//
+//  Created by Sergio TIMP on 17/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
